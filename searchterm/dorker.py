77adb614c251c76056d897315fb5985e34cde632
Lore <88983497+PyLore@users.noreply.github.com>
2022-03-28 00:15:25 -0400

    Update dorker.py

--- searchterm/dorker.py
+++ searchterm/dorker.py
@@ -1,6 +1,6 @@
 import requests, re
 
-from .exceptions import CaptchaError,ResultsError
+from .exceptions import CaptchaError,ResultsError,RequestFailure,QueryError
 """
 CREDITS::
 ---------------------------------------
@@ -23,39 +23,37 @@ searchterm.google(query = 'QUERY_GOES_HERE', proxy = 'http://127.0.0.1:8080')
 ---------------------------------------
 """
 class Google_Dorker:
-
+    blacklisted_urls = [
+                       'https://maps.google.com/maps',
+                       'https://support.google.com/websearch',
+                       'https://accounts.google.com/ServiceLogin',
+                       'https://www.google.com/search',
+                       'https://www.google.com/preferences',
+                       'https://policies.google.com/privacy',
+                       'https://policies.google.com/terms',
+                       'https://www.youtube.com/watch'
+                       ]
     def __init__(self, query = None, gdork = None, proxy = None):
-        """Query gets set here if present."""
+        #Query gets set here if present.
+        if query == '':
+            raise QueryError
         self.query    = query
         if gdork:
-           """Google Dork gets set here if present."""
+           #Google Dork gets set here if present.
            self.gdork = gdork
         else:
-           """Otherwise we use this dork."""
+           #Otherwise we use this dork.
            self.gdork = f'intext:"{self.query}"'
-        """Apply proxy IF present."""   
+        #Apply proxy IF present.   
         self.proxy    = proxy
-        """Blacklist Useless URLS."""
-        Google_Dorker.blacklisted_urls = [
-                                         'https://maps.google.com/maps',
-                                         'https://support.google.com/websearch',
-                                         'https://accounts.google.com/ServiceLogin',
-                                         'https://www.google.com/search',
-                                         'https://www.google.com/preferences',
-                                         'https://policies.google.com/privacy',
-                                         'https://policies.google.com/terms',
-                                         'https://www.youtube.com/watch'
-                                         ]
         
     @staticmethod
-    def headers():    
-        """Scrape our headers."""
-        session     = requests.Session()
-        s           = session.get('http://www.google.com')
-        cookie      = ''
-        """Apply Each Value Into Our Cookie."""
-        for key, value in s.cookies.items():
-            cookie += f'{key}={value}; '
+    def _headers():    
+        cookie = ''
+        r      = requests.get('http://www.google.com')
+        #Apply Each Value Into Our Cookie.
+        for key, value in r.cookies.items(): 
+            cookie = ''.join(f'{key}={value};')  
         return  {
                 'Host'           : 'www.google.com',
                 'Referer'        : 'https://google.com/',
@@ -63,70 +61,44 @@ class Google_Dorker:
                 'Accept-Language': 'en-US,en;q=0.5',
                 'Accept-Encoding': 'gzip, deflate, br',
                 'User-Agent'     : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Safari/536.5',
-                'Connection'     : 'keep-alive',
+                'Connection'     : 'close',
                 'Cookies'        : cookie
                 }
 
     @staticmethod
-    def detect_captcha(r):
-       """ If google blocks the request we return True"""
-       if 'Our systems have detected unusual traffic from your computer network' in r:
-           return True
-       return False   
+    def _detect_captcha(self):
+       #If google blocks the request.
+       return 'Our systems have detected unusual traffic from your computer network' in self.r
            
     @staticmethod
-    def format_url(url):
-       """Remove useless strings if present."""
-       if '/url?q=' in url:
-           url = url.replace('/url?q=','')
-       if '&sa=' in url:    
-           url = url.split('&sa=')[0]
+    def _format_url(url):
+       #Remove useless strings if present.
+       if '/url?q=' in url: url = url.replace('/url?q=','')
+       if '&sa=' in url: url    = url.split('&sa=')[0]
        return url
        
-    @staticmethod
-    def read_html(r):
+    def _read_html(self):
+       if self._detect_captcha(self): raise CaptchaError
        urls = []
-       """Check for google captcha"""
-       if Google_Dorker.detect_captcha(r):
-           raise CaptchaError
-       """Otherwise continue""" 
-       links = re.findall(r'http[s*]:[a-zA-Z0-9_.+-/#~]+',r)
-       for link in links:
-          """If 'http' is present in link."""
-          if 'http' in link:
-              """If The Link Is In The Blacklist."""
-              if link in Google_Dorker.blacklisted_urls:
-                pass
-              else:  
-                  """If The Link Is Already In The List urls."""
-                  if link in urls:
-                      pass
-                  else:  
-                      url = Google_Dorker.format_url(link)
-                      urls.append(url)
-       return urls    
+       for link in re.findall(r'http[s*]:[a-zA-Z0-9_.+-/#~]+', self.r):
+          if not (link in Google_Dorker.blacklisted_urls and urls) and 'http' in link:
+                urls.append(self._format_url(link))
+       if not urls: raise ResultsError
+       return urls 
 
-    def create_request(self):
-        """Constructs a request to google."""
+    def _create_request(self):
+        #Constructs a request to google.
         try:
-            """If Request Was A Success."""
+            #If Request Was A Success.
             session = requests.Session()
             return session.get(
                         url     = f'https://www.google.com/search?q={self.gdork}',
-                        headers = Google_Dorker.headers(),
+                        headers = self._headers(),
                         proxies = {'https': self.proxy}
                         ).text 
-        except Exception as e:
-            """If The Request Failed."""
-            return f"{e!r}"
+        except Exception as e: raise RequestFailure
             
-    def main(self):
-        """Executes all functions"""
-        r    = Google_Dorker.create_request(self)
-        """We Grab The URLS Now."""
-        urls = Google_Dorker.read_html(r)
-        """Return The URLS."""
-        if urls == []:
-            """If List Is Empty Raise Error."""
-            raise ResultsError
-        return urls
+    def _execute(self):
+        self.r  = self._create_request()
+        #Return The URLS.
+        return self._read_html()
